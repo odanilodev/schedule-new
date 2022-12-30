b13ca3b0c7f29c003c87eb5f4cbcf4959c1cedf6
Luis Felipe Lopes <99437129+oluisfelipedev@users.noreply.github.com>
2022-12-30 16:14:53 -0300

    create menu buttons without links (#7)
    Co-authored-by: root <root@LAPTOP-IBAQ50A2.localdomain>

--- resources/views/sidebar/sidebar.blade.php
+++ resources/views/sidebar/sidebar.blade.php
@@ -25,22 +25,69 @@
                     </a>
                 </div>
             </li>
-            <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+
+            <li>
+                <a href="" aria-expanded="false">
                     <i class="flaticon-025-dashboard"></i>
                     <span class="nav-text">Painel</span>
                 </a>
+            </li>
+
+            <li>
+                <a href="" aria-expanded="false">
+                    <i class="bi bi-calendar3"></i>
+                    <span class="nav-text">Agenda</span>
+                </a>
+            </li>
+
+            <li>
+                <a href="" aria-expanded="false">
+                    <i class="bi bi-people-fill"></i>
+                    <span class="nav-text">Clientes</span>
+                </a>
+                
+            </li>
+
+            <li>
+                <a href="" aria-expanded="false">
+                    <i class="bi bi-tools"></i>
+                    <span class="nav-text">Serviços</span>
+                </a>
+                
+            </li>
+
+            <li>
+                <a class="has-arrow ai-icon" href="" aria-expanded="false">
+                    <i class="bi bi-basket-fill"></i>
+                    <span class="nav-text">Produtos</span>
+                </a>
                 <ul aria-expanded="false">
-                    <li><a href="{{ route('user/table') }}">Usuários</a></li>
+                    <li>
+                        <a href=""><i class="bi bi-list-ul"></i> Listagem </a>
+                    </li>
+
+                    <li>
+                        <a href=""><i class="bi bi-tag-fill"></i> Categorias </a>
+                    </li>
                 </ul>
             </li>
-            <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
-                <i class="flaticon-072-printer"></i>
-                <span class="nav-text">Formulários</span>
-            </a>
-            <ul aria-expanded="false">
-                <li><a href="{{ route('form/input/new') }}">Formulário </a></li>
-            </ul>
-        </li>
+
+            <li>
+                <a href="" aria-expanded="false">
+                    <i class="bi bi-person-lines-fill"></i>
+                    <span class="nav-text">Profissionais</span>
+                </a>
+            </li>
+
+            <li>
+                <a href="" aria-expanded="false">
+                    <i class="bi bi-shop"></i>
+                    <span class="nav-text">Minhas Unidades</span>
+                </a>
+            </li>
+
+          
+
         </ul>
     </div>
 </div>
